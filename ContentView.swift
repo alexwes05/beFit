f4c4689f15de8dd077956ab25c9cc744f2da0531
Alex Wesolowski <alex109902@gmail.com>
2026-06-02 16:53:51 -0400

    Created homescreen and views

--- ContentView.swift
+++ ContentView.swift
@@ -10,10 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            
+        NavigationStack {
+            ZStack {
+                Color(red: 240/255, green: 240/255, blue: 255/255)
+                    .ignoresSafeArea(edges: .all)
+                Image("AppBackground")
+                    .renderingMode(.template)
+                    .foregroundStyle(.blue)
+                    .opacity(0.1)
+            VStack(spacing:20) {
+                       Text("BeFit")
+                           .font(Font.custom("Pixelify Sans", size: 80))
+                       
+                        NavigationLink {
+                               CreateNewSplit()
+                           } label: {
+                               Text("Create New Split")
+                           }.buttonStyle(BorderedButtonStyle())
+                            .font(Font.custom("Pixelify Sans", size: 20))
+                            .foregroundColor(.black)
+                            .tint(.blue)
+                
+                        NavigationLink {
+                               ViewWorkouts()
+                           } label: {
+                               Text("View my workouts")
+                           }
+                           .buttonStyle(BorderedButtonStyle())
+                           .font(Font.custom("Pixelify Sans", size: 20))
+                           .foregroundColor(.black)
+                           .tint(.gray)
+                   }
+            }
         }
+        
     }
+    
 }
 
 

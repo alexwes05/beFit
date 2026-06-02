7fcf06abfb85b985752cdeff143fa4eaaa037b29
Alex Wesolowski <alex109902@gmail.com>
2026-06-02 15:14:26 -0400

    Initial Commit

---
+++ ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BeFit
+//
+//  Created by Alex Wesolowski on 4/24/26.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack{
+            
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
+
